2eb8f3f97054ce6dbd6a50f621533a10feca8e53
Virtue1040 <rafi.hidayat1313@gmail.com>
2025-03-10 22:26:11 +0700

    Add Comment

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,36 @@ import java.util.*;
 public class Main {
     private static LinkedList<Buku> listBuku = new LinkedList<>();
 
+    /**
+     * Berfungsi untuk menambahkan ke LinkedList awal
+     * @param isbn
+     * @param judul
+     * @param kategori
+     * @param hargaPinjam
+     * @param status
+     */
     private static void addFirstBuku(int isbn, String judul, String kategori, int hargaPinjam, String status) {
         Buku buku = new Buku(isbn, judul, kategori, hargaPinjam, status);
         listBuku.addFirst(buku);
     }
 
+    /**
+     * Berfungsi untuk menambahkan ke LinkedList terakhir
+     * @param isbn
+     * @param judul
+     * @param kategori
+     * @param hargaPinjam
+     * @param status
+     */
     private static void addLastBuku(int isbn, String judul, String kategori, int hargaPinjam, String status) {
         Buku buku = new Buku(isbn, judul, kategori, hargaPinjam, status);
         listBuku.addLast(buku);
     }
 
+    /**
+     * Berfungsi untuk menghapus buku berdasarkan isbn
+     * @param isbn
+     */
     private static void hapusBuku(int isbn) {
         if (listBuku.isEmpty()) {
             return;
@@ -28,6 +48,9 @@ public class Main {
         }
     }
 
+    /**
+     * Berfungsi untuk mencetak isi listBuku dari depan dan belakang
+     */
     private static void print() {
         System.out.println("print buku dari depan : ");
         if (listBuku.isEmpty()) {
@@ -48,6 +71,15 @@ public class Main {
         System.out.println();
     }
 
+    /**
+     * Berfungsi untuk mengedit buku berdasarkan isbn, dan akan mengubah informasi umum seperti
+     * judul, kategori, hargaPinjam, dan status
+     * @param isbn
+     * @param judul
+     * @param kategori
+     * @param hargaPinjam
+     * @param status
+     */
     private static void editBuku(int isbn, String judul, String kategori, int hargaPinjam, String status) {
         if (listBuku.isEmpty()) {
             return;
@@ -66,16 +98,23 @@ public class Main {
     }
 
     public static void main(String[] args) {
+        // Nambah 5 data buku awal //
+
+        // 2 Add First
         addFirstBuku(10, "Indonesia", "Sejarah", 10000, "Tersedia");
         addFirstBuku(12, "German", "Sejarah", 10000, "Tersedia");
+
         addLastBuku(15, "Russia", "Sejarah", 10000, "Tersedia");
         addLastBuku(18, "Inggris", "Sejarah", 10000, "Tersedia");
         addLastBuku(20, "Irsale", "Sejarah", 10000, "Tersedia");
 
+        // Delete berdasarkan ISBN
         hapusBuku(12);
 
+        // Print dapat menampilkan list dari depan dan belakang
         print();
 
+        // Edit 2 data terakhir dari list
         editBuku(20, "Irsale", "Sejarah", 10000, "Terpinjam");
         editBuku(18, "Eropa", "Sejarah", 10000, "Terpinjam");
 
